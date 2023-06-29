c1b8ef8ebcc2a3abbf6e6c64986c2d2701d184f4
kevicebryan <kevinbryanreligion@gmail.com>
2023-06-29 11:28:42 +0700

    add: ContentView for SwiftUI

---
+++ Lug-N-Loaded/Views/ContentView.swift
@@ -0,0 +1,34 @@
+//
+//  ContentView.swift
+//  Lug-N-Loaded
+//
+//  Created by Kevin Bryan on 29/06/23.
+//
+
+import SpriteKit
+import SwiftUI
+
+
+//TODO: Fix bug di obstruction -> kecilin text dikit + angka ke-4 g muncul!
+
+struct ContentView: View {
+  var scene: SKScene {
+    let scene = GameScene()
+    scene.size = UIScreen.main.bounds.size
+    scene.scaleMode = .fill
+    return scene
+  }
+
+  var body: some View {
+    SpriteView(scene: scene)
+      .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
+      .ignoresSafeArea()
+  }
+}
+
+//
+// struct ContentView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        ContentView()
+//    }
+// }
